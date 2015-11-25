f11f4824d184742290e49777b7e317a4bd9612ed
Franz Skaaning <fs@bellcom.dk>
2015-11-25 14:05:51 +0100

    If sætninger med i default panels layout

--- public_html/profiles/os2sub/themes/bellcom/layouts/content-with-left-and-right-sidebar/content-with-left-and-right-sidebar.tpl.php
+++ public_html/profiles/os2sub/themes/bellcom/layouts/content-with-left-and-right-sidebar/content-with-left-and-right-sidebar.tpl.php
@@ -1,22 +1,48 @@
 <!-- content-with-left-and-right-sidebar.tpl.php -->
 <div class="row" <?php if (!empty($css_id)) { print "id=\"$css_id\""; } ?>>
 
-    <!-- Begin - left sidebar -->
-    <div class="col-md-3">
-        <?php print $content['sidebar-left']; ?>
-    </div>
-    <!-- End - left sidebar -->
+    <?php if ($content['sidebar-left']): ?>
+	    <!-- Begin - left sidebar -->
+	    <div class="col-md-3">
+	        <?php print $content['sidebar-left']; ?>
+	    </div>
+	    <!-- End - left sidebar -->
+	<?php endif ?>
 
-    <!-- Begin - right sidebar -->
-    <div class="col-md-3 col-md-push-6">
-        <?php print $content['sidebar-right']; ?>
-    </div>
-    <!-- End - right sidebar -->
+    <?php if ($content['sidebar-right']): ?>
+    
+	    <!-- Begin - right sidebar -->
+	    <div class="col-md-3 col-md-push-6">
+	        <?php print $content['sidebar-right']; ?>
+	    </div>
+	    <!-- End - right sidebar -->
+	<?php endif ?>
 
-    <!-- Begin - content -->
-    <div class="col-md-6 col-md-pull-3">
-        <?php print $content['content']; ?>
-    </div>
-    <!-- End - content -->
+	<?php if ($content['sidebar-left'] && $content['sidebar-right']): ?>
+    	
+    	<?php /* If both sidebars are present*/?>
+    	
+	    <!-- Begin - content -->
+	    <div class="col-md-6 col-md-pull-3">
+	        <?php print $content['content']; ?>
+	    </div>
+	    <!-- End - content -->
 
+	<?php elseif ($content['sidebar-left']): ?>
+
+    	<?php /* If left sidebar is present*/?>
+
+		<div class="col-md-9">
+	        <?php print $content['content']; ?>
+	    </div>
+
+	<?php else : ?>
+
+    	<?php /* If no sidebars are present*/?>
+	
+		<div class="col-sm-12">
+	        <?php print $content['content']; ?>
+	    </div>
+
+	<?php endif ?>
 </div>
