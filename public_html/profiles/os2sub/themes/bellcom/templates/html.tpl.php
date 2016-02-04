d3ca8464ad5286bf63baab3a695b69be5631d3b6
Franz Skaaning <fs@bellcom.dk>
2016-02-04 11:42:22 +0100

    Rem touch icons

--- public_html/profiles/os2sub/themes/bellcom/templates/html.tpl.php
+++ public_html/profiles/os2sub/themes/bellcom/templates/html.tpl.php
@@ -18,26 +18,6 @@
   <?php print $styles; ?>
   <!-- End - internal stylesheet -->
 
-  <!-- Begin - icons -->
-  <link rel="apple-touch-icon" sizes="57x57" href="<?php print $path_img; ?>/icons/apple-touch-icon-57x57.png">
-  <link rel="apple-touch-icon" sizes="60x60" href="<?php print $path_img; ?>/icons/apple-touch-icon-60x60.png">
-  <link rel="apple-touch-icon" sizes="72x72" href="<?php print $path_img; ?>/icons/apple-touch-icon-72x72.png">
-  <link rel="apple-touch-icon" sizes="76x76" href="<?php print $path_img; ?>/icons/apple-touch-icon-76x76.png">
-  <link rel="apple-touch-icon" sizes="114x114" href="<?php print $path_img; ?>/icons/apple-touch-icon-114x114.png">
-  <link rel="apple-touch-icon" sizes="120x120" href="<?php print $path_img; ?>/icons/apple-touch-icon-120x120.png">
-  <link rel="apple-touch-icon" sizes="144x144" href="<?php print $path_img; ?>/icons/apple-touch-icon-144x144.png">
-  <link rel="apple-touch-icon" sizes="152x152" href="<?php print $path_img; ?>/icons/apple-touch-icon-152x152.png">
-  <link rel="apple-touch-icon" sizes="180x180" href="<?php print $path_img; ?>/icons/apple-touch-icon-180x180.png">
-  <link rel="icon" type="image/png" href="<?php print $path_img; ?>/icons/favicon-32x32.png" sizes="32x32">
-  <link rel="icon" type="image/png" href="<?php print $path_img; ?>/icons/favicon-96x96.png" sizes="96x96">
-  <link rel="icon" type="image/png" href="<?php print $path_img; ?>/icons/favicon-16x16.png" sizes="16x16">
-  <link rel="manifest" href="<?php print $path_img; ?>/icons/manifest.json">
-  <link rel="mask-icon" href="<?php print $path_img; ?>/icons/safari-pinned-tab.svg" color="#5bbad5">
-  <meta name="msapplication-TileColor" content="#da532c">
-  <meta name="msapplication-TileImage" content="<?php print $path_img; ?>/icons/mstile-144x144.png">
-  <meta name="theme-color" content="#ffffff">
-  <!-- End - icons -->
-
 </head>
 <body class="<?php print $classes; ?>"<?php print $attributes; ?>>
 
