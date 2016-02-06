b22e90dabc1d79db82c02d7d8b7fd29562bf7524
Franz Skaaning <fs@bellcom.dk>
2016-02-06 21:54:25 +0100

    Spotbokse uden links

--- public_html/profiles/os2sub/themes/bellcom/templates/node/spotbox/node--os2web-spotbox-box--spotbox.tpl.php
+++ public_html/profiles/os2sub/themes/bellcom/templates/node/spotbox/node--os2web-spotbox-box--spotbox.tpl.php
@@ -22,11 +22,27 @@
             $path = drupal_get_path_alias('node/' . $node->nid);
               
             print $html = '<img class="img-responsive img-rounded" title = "' . $image["title"] . '" src="' . $public_filename . '"/>'; ?>
-              
+        
+        
+        <?php if ($node->field_spot_link['und']['0']['title'] !== $node->field_spot_link['und']['0']['url']) : ?>
+          <span class="os2-node-teaser-heading">
+      			<span class="os2-node-teaser-heading-title">
+      				<?php print $node->field_spot_link['und']['0']['title']; ?>
+      			</span>
+      		</span>
+        <?php endif; ?>
+        
 		<?php else: ?>
-
+    <div class="panel-pane pane-views pane-os2sub-spotboks">
+      <div class="os2sub-box">
 		    <?php if(!empty($content['field_os2web_spotbox_text'])) : ?>
-		
+          <?php if ($node->field_spot_link['und']['0']['title'] !== $node->field_spot_link['und']['0']['url']) : ?>
+            <span class="os2-node-teaser-heading">
+        			<span class="os2-node-teaser-heading-title">
+        				<?php print $node->field_spot_link['und']['0']['title']; ?>
+        			</span>
+        		</span>
+          <?php endif; ?>
 		    <span class="os2-node-teaser-body">
 		      <span class="os2-node-teaser-body-content">
 		          <?php print $node->field_os2web_spotbox_text['und']['0']['safe_value']; ?>
@@ -34,17 +50,15 @@
 		    </span>
 	
 		    <?php endif; ?>
+        </div>
+      </div>
 
-   	    <?php endif; ?>
+   	  <?php endif; ?>
   		
-  		<span class="os2-node-teaser-heading">
-  			<span class="os2-node-teaser-heading-title">
-  				<?php print $node->field_spot_link['und']['0']['title']; ?>
-  			</span>
-  		</span>
-  	<?php if (isset($node->field_spot_link['und']['0']['url'])) : ?>
-    </a>
-	<?php endif; ?>
+
+      <?php if (isset($node->field_spot_link['und']['0']['url'])) : ?>
+      </a>
+      <?php endif; ?>
    </article> <!-- /.node -->
 
 <?php endif; ?>
