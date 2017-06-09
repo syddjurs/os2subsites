3347a57dbab21252748cd70bd4c6a3403a03d1a3
Franz Skaaning <fs@bellcom.dk>
2017-06-09 10:38:08 +0200

    Velkomst tpl

--- public_html/profiles/os2sub/themes/subsite_2/templates/node--velkomst--full.tpl.php
+++ public_html/profiles/os2sub/themes/subsite_2/templates/node--velkomst--full.tpl.php
@@ -4,31 +4,20 @@
 
   <div id="node-<?php print $node->nid; ?>" class="<?php print $classes; ?> subsite-full"<?php print $attributes; ?>>
  
-    <div class="subsite-full-heading">
+    <div class="os2sub-teaser-heading">
+      
       <?php print render($title_prefix); ?>
-      <h2<?php print $title_attributes; ?> class="subsite-full-heading-title"><?php print $title; ?></h2>
+      
+      <h2<?php print $title_attributes; ?> class="os2sub-teaser-heading-title"><?php print $title; ?></h2>
+      
       <?php print render($title_suffix); ?>
       
-      <?php if (isset($content['field_os2web_base_field_image'])): ?>
-        <?php
-          // $field_image_all = field_get_items('node',$node,'field_os2web_base_field_image');
-          // $img_rendered = field_view_value('node',$node,'field_os2web_base_field_image',$field_image_all[0],
-
-          // array('type' => '$img_rendered','settings'=>array('image_style' => 'os2sub_normal_imagesize')));
-          // print render($img_rendered);
-          
-          print render($content['field_os2web_base_field_image']);
-          ?>
+      <?php if (isset($content['field_os2web_base_field_summary'])): ?>
+        <div class="lead">
+          <?php print render($content['field_os2web_base_field_summary']); ?>
+        </div>
       <?php endif; ?>
-
-
       
-          <?php if (isset($content['field_os2web_base_field_summary'])): ?>
-            <div class="lead">
-              <?php print render($content['field_os2web_base_field_summary']); ?>
-            </div>
-          <?php endif; ?>
-
     </div>
 
     <?php if (isset($content['field_os2web_base_field_intro'])): ?>
