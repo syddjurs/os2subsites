d47e37acd0b62a93f1c21528d2f1a4e7c4e4a71c
Morten Nissen <mail@hjemmesidekongen.dk>
2017-12-15 15:21:33 +0100

    Reformat Bellcom theme

--- public_html/profiles/os2sub/themes/bellcom/theme-settings.php
+++ public_html/profiles/os2sub/themes/bellcom/theme-settings.php
@@ -1,13 +1,10 @@
 <?php
-include( dirname(__FILE__) . '/include/settings.inc');
+include(dirname(__FILE__) . '/include/settings.inc');
 
 /**
  * Implements hook_form_FORM_ID_alter().
  */
-function bellcom_form_system_theme_settings_alter(&$form, $form_state, $form_id = NULL) {
-  if (isset($form_id)) {
-    return;
-  }
+function bellcom_form_system_theme_settings_alter(&$form, &$form_state) {
 
   // Settings form.
   _theme_settings_form($form, $form_state);
