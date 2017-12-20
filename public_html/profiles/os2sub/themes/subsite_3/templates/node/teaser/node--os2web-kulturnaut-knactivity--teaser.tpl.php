d6381a18cba6da9948cca57fa93e90f32f0eb776
Morten Nissen <mail@hjemmesidekongen.dk>
2017-12-20 16:23:44 +0100

    SYDDK-486 - event teaser

--- public_html/profiles/os2sub/themes/subsite_3/templates/node/teaser/node--os2web-kulturnaut-knactivity--teaser.tpl.php
+++ public_html/profiles/os2sub/themes/subsite_3/templates/node/teaser/node--os2web-kulturnaut-knactivity--teaser.tpl.php
@@ -2,16 +2,14 @@
     <!-- Begin - teaser -->
     <article id="node-<?php print $node->nid; ?>" class="<?php print $classes; ?> clearfix"<?php print $attributes; ?>>
 
-        <?php if ( isset( $content['field_image'] ) ): ?>
+        <?php if ( isset( $content['field_os2web_kulturnaut_slidesho'] ) ): ?>
             <!-- Begin - image -->
-            <div class="image">
-                <?php print render( $content['field_image'] ); ?>
+            <div class="entity-teaser__image">
+                <?php print render( $content['field_os2web_kulturnaut_slidesho'] ); ?>
             </div>
             <!-- End - image -->
         <?php endif; ?>
 
-        <?php print $node->title; ?>
-asdads
     </article>
     <!-- End - teaser -->
 <?php endif; ?>
