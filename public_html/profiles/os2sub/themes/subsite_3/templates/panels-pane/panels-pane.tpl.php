48fa61d0f3bac1f11e30b256a89b21519096efcc
Morten Nissen <mail@hjemmesidekongen.dk>
2018-01-02 15:46:07 +0100

    SYDDK-492 - panel variants

--- public_html/profiles/os2sub/themes/subsite_3/templates/panels-pane/panels-pane.tpl.php
+++ public_html/profiles/os2sub/themes/subsite_3/templates/panels-pane/panels-pane.tpl.php
@@ -1,4 +1,3 @@
-<!-- panels-pane--plain.tpl.php -->
 <?php if ($pane_prefix): ?>
   <?php print $pane_prefix; ?>
 <?php endif; ?>
@@ -9,30 +8,30 @@
 
   <?php print render($title_prefix); ?>
   <?php if ($title): ?>
-    <h2<?php print $title_attributes; ?>><?php print $title; ?></h2>
+      <h2<?php print $title_attributes; ?>><?php print $title; ?></h2>
   <?php endif; ?>
   <?php print render($title_suffix); ?>
 
   <?php if ($feeds): ?>
-    <div class="feed">
-      <?php print $feeds; ?>
-    </div>
+      <div class="feed">
+        <?php print $feeds; ?>
+      </div>
   <?php endif; ?>
 
-  <div class="pane-content">
-    <?php print render($content); ?>
-  </div>
+    <div class="pane-content">
+      <?php print render($content); ?>
+    </div>
 
   <?php if ($links): ?>
-    <div class="links">
-      <?php print $links; ?>
-    </div>
+      <div class="links">
+        <?php print $links; ?>
+      </div>
   <?php endif; ?>
 
   <?php if ($more): ?>
-    <div class="more-link">
-      <?php print $more; ?>
-    </div>
+      <div class="more-link">
+        <?php print $more; ?>
+      </div>
   <?php endif; ?>
 </div>
 <?php if ($pane_suffix): ?>
