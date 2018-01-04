a514567de7763b8baa774446434d2df801fc3dd7
Morten Nissen <mail@hjemmesidekongen.dk>
2018-01-04 08:59:27 +0100

    SYDDK-491 - panels layout

--- public_html/profiles/os2sub/themes/subsite_3/layouts/sectioned-top-light-dark-light-dark/sectioned-top-light-dark-light-dark.tpl.php
+++ public_html/profiles/os2sub/themes/subsite_3/layouts/sectioned-top-light-dark-light-dark/sectioned-top-light-dark-light-dark.tpl.php
@@ -2,11 +2,27 @@
   echo "id=\"$css_id\"";
 } ?>>
 
+  <?php if ($content['top']): ?>
+    <!-- Begin - top -->
+    <div class="panels-pane-region panels-pane-region--top">
+      <?php print $content['top']; ?>
+    </div>
+    <!-- End - top -->
+  <?php endif ?>
+
   <?php if ($content['light_section_1']): ?>
     <!-- Begin - light section no. 1 -->
     <div class="sectioned sectioned--light">
       <div class="sectioned__inner">
-        <?php echo $content['light_section_1']; ?>
+        <div class="container">
+          <div class="row">
+            <div class="col-xs-12">
+              <div class="panels-pane-region panels-pane-region--light-section-1">
+                <?php echo $content['light_section_1']; ?>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
     <!-- End - light section no. 1 -->
@@ -19,7 +35,9 @@
         <div class="container">
           <div class="row">
             <div class="col-xs-12">
-              <?php echo $content['dark_section_1']; ?>
+              <div class="panels-pane-region panels-pane-region--dark-section-1">
+                <?php echo $content['dark_section_1']; ?>
+              </div>
             </div>
           </div>
         </div>
@@ -35,7 +53,9 @@
         <div class="container">
           <div class="row">
             <div class="col-xs-12">
-              <?php echo $content['light_section_2']; ?>
+              <div class="panels-pane-region panels-pane-region--light-section-2">
+                <?php echo $content['light_section_2']; ?>
+              </div>
             </div>
           </div>
         </div>
@@ -44,14 +64,16 @@
     <!-- End - light section no. 2 -->
   <?php endif; ?>
 
-  <?php if ($content['dark_section_1']): ?>
+  <?php if ($content['dark_section_2']): ?>
     <!-- Begin - dark section no. 2 -->
     <div class="sectioned sectioned--dark">
       <div class="sectioned__inner">
         <div class="container">
           <div class="row">
             <div class="col-xs-12">
-              <?php echo $content['dark_section_2']; ?>
+              <div class="panels-pane-region panels-pane-region--dark-section-2">
+                <?php echo $content['dark_section_2']; ?>
+              </div>
             </div>
           </div>
         </div>
