c478dd2285e2789229a46cd67957ec4c5027162b
Morten Nissen <mail@hjemmesidekongen.dk>
2018-01-11 10:28:47 +0100

    SYDDK-search

--- public_html/profiles/os2sub/themes/subsite_3/templates/search/search-api-page-result.tpl.php
+++ public_html/profiles/os2sub/themes/subsite_3/templates/search/search-api-page-result.tpl.php
@@ -1,16 +1,31 @@
+<?php
+$image_style = 'os2sub_small_imagesize';
+$image_url = false;
+
+if ($item->item_type == 'node') {
+  $node = $item->node;
+
+  if (isset($node->field_os2web_kulturnaut_slidesho['und']['0']['uri']) && $uri = $node->field_os2web_kulturnaut_slidesho['und']['0']['uri']) {
+    $image_url = file_create_url(image_style_url($image_style, $uri));
+  }
+}
+
+else if ($item->item_type == 'taxonomy_term') {
+  $taxonomy_term = $item->taxonomy_term;
+
+  if (isset($taxonomy_term->field_os2web_base_field_image['und']['0']['uri']) && $uri = $taxonomy_term->field_os2web_base_field_image['und']['0']['uri']) {
+    $image_url = file_create_url(image_style_url($image_style, $uri));
+  }
+}
+?>
+
 <li class="search-result">
   <div class="row">
+
     <div class="hidden-xs col-sm-2">
-      <?php if ($result['node']->field_os2web_kulturnaut_slidesho): ?>
-        <?php
-        $search_image_uri = $result['node']->field_os2web_kulturnaut_slidesho['und'][0]['uri'];
-        $search_image_filepath = file_create_url($search_image_uri);
-        ?>
-        <div class="search-image">
-          <img src="<?php print $search_image_filepath; ?>" />
-        </div>
-      <?php endif; ?>
+      <img src="<?php print $image_url; ?>" alt="" />
     </div>
+
     <div class="col-sm-10">
       <h3 class="title">
         <?php print $url ? l($title, $url['path'], $url['options']) : check_plain($title); ?>
