03bbacd8c09364ab038218bd567a1d2ac34ca896
Morten Nissen <mail@hjemmesidekongen.dk>
2018-01-11 14:33:14 +0100

    SYDDK-share

---
+++ public_html/profiles/os2sub/themes/subsite_3/templates/block/os2sub-social-share.tpl.php
@@ -0,0 +1,54 @@
+<?php
+$title = $variables['current_node']['title'];
+$url = $variables['current_node']['url'];
+?>
+
+<!-- Begin - share -->
+<div class="share">
+
+  <div class="share__item">
+
+    <!-- Begin - button -->
+    <button type="button" class="btn btn-default share__item__toggle">
+      <span class="fa fa-share"></span> Del opslaget
+    </button>
+    <!-- End - button -->
+
+    <!-- Begin - popup -->
+    <div class="share__item__popup">
+      <div class="share__item__social">
+        <a class="share__item__social__item share__item__social__item--twitter" href="http://twitter.com/intent/tweet?status=<?php print $title; ?>+<?php print $url; ?>" title="Del på Twitter" target="_blank">
+          <span class="icon fa fa-twitter"></span>
+        </a>
+
+        <a class="share__item__social__item share__item__social__item--facebook" href="http://www.facebook.com/sharer/sharer.php?u=<?php print $url; ?>&amp;title=<?php print $title; ?>" title="Del på Facebook" target="_blank">
+          <span class="icon fa fa-facebook"></span>
+        </a>
+
+        <a class="share__item__social__item share__item__social__item--linkedin" href="http://www.linkedin.com/shareArticle?mini=true&amp;url=<?php print $url; ?>&amp;title=<?php print $title; ?>" title="Del på LinkedIn" target="_blank">
+          <span class="icon fa fa-linkedin"></span>
+        </a>
+      </div>
+    </div>
+    <!-- End - popup -->
+
+  </div>
+
+  <div class="share__item">
+
+    <!-- Begin - print -->
+    <button type="button" class="btn btn-default" onclick="printDOM();">
+      <span class="fa fa-print"></span> Print
+    </button>
+    <!-- End - print -->
+
+  </div>
+
+</div>
+<!-- End - share -->
+
+<script>
+    function printDOM() {
+        window.print();
+    }
+</script>
