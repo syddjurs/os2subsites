cb430bd99dc743f6cf594c92e5db97c7985b868a
Morten Nissen <mail@hjemmesidekongen.dk>
2018-01-12 08:52:28 +0100

    SYDDK-velkomst

--- public_html/profiles/os2sub/themes/subsite_3/templates/node/teaser/node--teaser.tpl.php
+++ public_html/profiles/os2sub/themes/subsite_3/templates/node/teaser/node--teaser.tpl.php
@@ -1,7 +1,17 @@
 <?php if ($view_mode == 'teaser'): ?>
     <!-- Begin - teaser -->
     <a href="<?php print $node_url; ?>" id="node-<?php print $node->nid; ?>" class="<?php print $classes; ?> element-wrapper-link clearfix"<?php print $attributes; ?>>
+
+      <div class="entity-teaser__heading">
+        <h2 class="entity-teaser__heading__title">
+          <?php print $title; ?>
+        </h2>
+      </div>
+
+      <div class="entity-teaser__body">
         <?php print render($content); ?>
+      </div>
+
     </a>
     <!-- End - teaser -->
 <?php endif; ?>
