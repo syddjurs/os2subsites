250661af9543423c75f75d52b1c81dd94d97e57e
Franz Skaaning <fs@bellcom.dk>
2018-05-30 11:46:00 +0200

    sharer links

--- public_html/profiles/os2sub/themes/subsite_3/templates/block/os2sub-social-share.tpl.php
+++ public_html/profiles/os2sub/themes/subsite_3/templates/block/os2sub-social-share.tpl.php
@@ -19,19 +19,19 @@ $url = $variables['current_node']['url'];
       <div class="share__item__social-list">
 
         <!-- Begin - twitter -->
-        <a class="share__item__social-list__item share__item__social-list__item--twitter" href="http://twitter.com/intent/tweet?status=<?php print $title; ?>+<?php print $url; ?>" title="Del på Twitter" target="_blank">
+        <a class="share__item__social-list__item share__item__social-list__item--twitter" href="https://twitter.com/intent/tweet?status=<?php print $title; ?>+<?php print $url; ?>" title="Del på Twitter" target="_blank">
           <span class="icon fa fa-twitter"></span>
         </a>
         <!-- End - twitter -->
 
         <!-- Begin - facebook -->
-        <a class="share__item__social-list__item share__item__social-list__item--facebook" href="http://www.facebook.com/sharer/sharer.php?u=<?php print $url; ?>&amp;title=<?php print $title; ?>" title="Del på Facebook" target="_blank">
+        <a class="share__item__social-list__item share__item__social-list__item--facebook" href="https://www.facebook.com/sharer/sharer.php?u=<?php print $url; ?>&amp;title=<?php print $title; ?>" title="Del på Facebook" target="_blank">
           <span class="icon fa fa-facebook"></span>
         </a>
         <!-- End - facebook -->
 
         <!-- Begin - linkedin -->
-        <a class="share__item__social-list__item share__item__social-list__item--linkedin" href="http://www.linkedin.com/shareArticle?mini=true&amp;url=<?php print $url; ?>&amp;title=<?php print $title; ?>" title="Del på LinkedIn" target="_blank">
+        <a class="share__item__social-list__item share__item__social-list__item--linkedin" href="https://www.linkedin.com/shareArticle?mini=true&amp;url=<?php print $url; ?>&amp;title=<?php print $title; ?>" title="Del på LinkedIn" target="_blank">
           <span class="icon fa fa-linkedin"></span>
         </a>
         <!-- End - linkedin -->
