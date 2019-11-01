6ad70f43668705376517c06f43349470de6a3517
juuliabellcom <juuliadevadze@gmail.com>
2019-11-01 14:18:23 +0200

    Update Facebook Likebox 7.x-2.2-> 7.x-2.3

--- public_html/profiles/os2sub/modules/contrib/fb_likebox/fb_likebox.js
+++ public_html/profiles/os2sub/modules/contrib/fb_likebox/fb_likebox.js
@@ -1,11 +1,20 @@
-(function($) {
+Drupal.behaviors.fb_likebox = {
+  attach: function (context, settings) {
+		if (context !== document) {
+		  // AJAX request.
+		  return;
+		}
 
-	(function(d, s, id) {
-	  var js, fjs = d.getElementsByTagName(s)[0];
-	  if (d.getElementById(id)) return;
-	  js = d.createElement(s); js.id = id;
-	  js.src = "//connect.facebook.net/en_EN/sdk.js#xfbml=1&version=v2.3&appId=579264578881928";
-	  fjs.parentNode.insertBefore(js, fjs);
-	}(document, 'script', 'facebook-jssdk'));
-
-}(jQuery));
+		(function(d, s, id) {
+		  var js, fjs = d.getElementsByTagName(s)[0];
+		  if (d.getElementById(id)) return;
+		  js = d.createElement(s);
+		  js.id = id;
+		  js.src = "//connect.facebook.net/" + settings.fb_likebox_language + "/sdk.js#xfbml=1&version=v2.3";
+		  if (settings.fb_likebox_app_id) {
+			js.src += "&appId=" + settings.fb_likebox_app_id;
+		  }
+		  fjs.parentNode.insertBefore(js, fjs);
+		}(document, 'script', 'facebook-jssdk', settings));
+	}
+};
